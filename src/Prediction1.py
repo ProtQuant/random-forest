eb33928ea59892fd25e72e7603d64544c1c63e2a
Arnoldzyf <1923335670@qq.com>
2021-07-19 15:44:18 +0100

    use scores from 60 .csv files

--- src/Prediction1.py
+++ src/Prediction1.py
@@ -13,6 +13,7 @@ df_f = pd.read_csv(feature_file, sep='\t')  # chunksize=1000000 or iterator if t
 # print(df)
 
 # replace tne 'na' with 0. They are string, not really NaN
+df_f.fillna(0, inplace=True)
 df_f.replace('na', 0, inplace=True)
 # print(df)
 
@@ -27,7 +28,7 @@ df_f['NAME'] = df_f.apply(lambda x: x['NAME'][9:], axis=1)
 """
 read in the scores
 """
-score_file = '../../score-peptides/Testcases4/T2/output/score.csv'
+score_file = '../../score-peptides/output/60_score.csv'
 df_s = pd.read_csv(score_file, sep=',')
 # print(df_s)
 
@@ -43,13 +44,38 @@ df_f.set_index(['NAME'], inplace=True)  # should have no duplicate peptides
 df_s.set_index(['peptide'], inplace=True)
 # print(df_s)
 df = pd.merge(df_s, df_f, left_index=True, right_index=True)
-print(df)
+print('size of [#pep, #label+feature]: -------')
+print(df.shape)
 # find out the distribution of scores
+print('distribution of labels: ------')
 print(df['score'].value_counts())
-"""
-data screening is needed here
-"""
 
+"""
+data screening 
+"""
+# check data type of each column
+# pd.set_option('display.max_rows', 6000)
+# print(df.dtypes)
+
+# may remove columns whose dtype is object (a mixture of int64 and float64)
+# df = df._get_numeric_data()
+# print(df.shape)
+
+# cast int into float, or dtype of a column may be object
+df = df.astype('float64')
+# print(df.dtypes)
+
+#  delete the columns has only one value, can be done earlier when read in features file
+# df = df.loc[:, (df != df.iloc[0]).any()]
+# print(df.std(ddof=0) == 0)
+df.drop(df.columns[df.std(ddof=0) == 0], axis=1, inplace=True)
+print('size after drop columns having only one value: ------')
+print(df.shape)  # 5271 --> 2039
+
+# delete duplicate columns
+df = df = df.T.drop_duplicates().T
+print('size after drop duplicate columns: ------')
+print(df.shape)
 """
 build train and test data set
 """
@@ -57,6 +83,7 @@ label = 'score'
 y = df[label]
 X = df.drop(label, 1)
 X_train, X_test, y_train, y_test = train_test_split(X, y)
+
 # print(X_train.shape)
 # print(y_train.shape)
 # print(X_test.shape)
@@ -65,16 +92,25 @@ X_train, X_test, y_train, y_test = train_test_split(X, y)
 """
 Random forest regression
 """
-# num_trees = 200
-# Rf = RandomForestRegressor(n_estimators=num_trees, max_depth=None)
-#
-# Rf.fit(X_train, Y_train)
-# y_ = Rf.predict(X_test)
-#
-# print(y_, Y_test)
-# print(r2_score(Y_test, y_))
-#
-# x = range(len(Y_test))
-# plt.scatter(x, Y_test )
-# plt.scatter(x, y_)
-# plt.show()
+num_trees = 200
+Rf = RandomForestRegressor(n_estimators=num_trees, max_depth=None)
+
+Rf.fit(X_train, y_train)
+y_ = Rf.predict(X_test)
+
+# importance of features
+features = df.columns[1:]
+importances = Rf.feature_importances_
+indices = np.argsort(importances)[::-1]
+for f in range(X_train.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30, features[indices[f]], importances[indices[f]]))
+
+# print(y_, y_test)
+print(r2_score(y_test, y_))
+
+x = range(len(y_test))
+plt.scatter(x, y_test )
+plt.scatter(x, y_)
+plt.show()
+
+
